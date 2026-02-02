988840f09794b752704d308100eeb25419eb6919
HarryHosono <139094077+HarryHosono@users.noreply.github.com>
2026-02-02 12:50:27 +0900

    Create smartC.cpp

---
+++ ABC443/smartC.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, t;
+  cin >> n >> t;
+  vector<int> a(n + 1);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  // 次にchokutterを開く時間
+  int nxt = 0; 
+  int ans = 0;
+  // T秒に青木さんが後ろを通ったとする
+  a[n] = t;
+  for (int i = 0; i <= n; i++) {
+    // nxtよりも青木さんが後ろを通った時の値が小さい場合→通った時間-開き始めた時間(nxt)が開いていた時間
+    if (nxt <= a[i]) {
+      ans += a[i] - nxt;
+      nxt = a[i] + 100;
+    } 
+  }
+  cout << ans << '\n';
+  return 0;
+}
